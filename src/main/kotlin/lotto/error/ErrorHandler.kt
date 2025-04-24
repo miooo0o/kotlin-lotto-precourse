a92f0afa95e7fd0487e8fb27b678499ea3baf1a8
Mina Kim <mmmiiinn@me.com>
2025-04-24 15:29:30 +0200

    feat(error): implement ErrorHandler to throw on Retryable ErrorTypes

--- src/main/kotlin/lotto/error/ErrorHandler.kt
+++ src/main/kotlin/lotto/error/ErrorHandler.kt
@@ -1,5 +1,25 @@
 package lotto.error
 
-class ErrorHandler {
-
+class RetryInputException(message: String) : IllegalArgumentException(message) {
 }
+
+class LogicError(message: String) : IllegalStateException(message) {
+}
+
+object ErrorHandler {
+	fun check(errorType: ErrorType) {
+		if (errorType == Common.NON_ERROR) return
+		if (errorType.isTypeRetry()) throw RetryInputException(ErrorFormatter.of(errorType))
+		if (errorType.isTypeLogic()) throw LogicError("error handler -> ${errorType::class.simpleName}") // TODO: better message
+	}
+}
+
+fun ErrorType.isTypeRetry(): Boolean =
+	this is Winning || this is Bonus || this is Purchase
+
+fun ErrorType.isTypeLogic(): Boolean =
+	this is Logic
+
+fun ErrorType?.isStatusSuccess(): Boolean = this == null
+
+fun ErrorType?.isStatusFailure(): Boolean = this != null
