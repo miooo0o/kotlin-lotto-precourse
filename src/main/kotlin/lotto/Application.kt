93ee151f5620705bb746b6e4448ae2c0090abbc7
Mina Kim <mmmiiinn@me.com>
2025-04-25 23:14:36 +0200

    feat(app): connect input flow into main application
    - Integrated repeatUntilValid() into main()
    - Winning numbers, bonus number, and purchase amount are now read from user input
    - Prepares for ticket generation

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,19 @@
 package lotto
 
+import lotto.error.UnexpectedException
+import lotto.policy.GamePolicy
+import lotto.view.InputView
+import lotto.view.util.repeatUntilValid
+
 fun main() {
-    // TODO: Implement the program
+
+	try {
+		
+		val winning = repeatUntilValid(GamePolicy.WINNING_MESSAGE) { InputView.parseWinningNumbersOrThrow(it) }
+		val bonus = repeatUntilValid(GamePolicy.BONUS_MESSAGE) { InputView.parseBonusNumberOrThrow(it) }
+		val amount = repeatUntilValid(GamePolicy.AMOUNT_MESSAGE) { InputView.parseAmountOrThrow(it) }
+
+	} catch (e: UnexpectedException) {
+
+	}
 }
