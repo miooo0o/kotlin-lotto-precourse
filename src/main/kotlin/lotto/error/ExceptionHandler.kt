0410ae9d4d42f14f6f626606cb77d27499c2d929
Mina Kim <mmmiiinn@me.com>
2025-04-25 18:33:37 +0200

    chore(error) remove unused exception due to logic refactor

--- src/main/kotlin/lotto/error/ExceptionHandler.kt
+++ src/main/kotlin/lotto/error/ExceptionHandler.kt
@@ -6,9 +6,6 @@ class RetryInputException(message: String) : IllegalArgumentException(message) {
 class UnexpectedException(message: String) : IllegalStateException(message) {
 }
 
-class UnreachableCodeException(message: String) : IllegalStateException(message) {
-}
-
 object ExceptionHandler {
 	fun throwIf(errorType: ErrorType) {
 		if (errorType == Common.NON_ERROR) return
