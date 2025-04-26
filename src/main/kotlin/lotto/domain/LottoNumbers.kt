5dfcb71beccd17bcb9529efb1307c3356c607098
Mina Kim <mmmiiinn@me.com>
2025-04-26 22:23:00 +0200

    feat(domain): add LottoNumbers generator for random lotto number creation

---
+++ src/main/kotlin/lotto/domain/LottoNumbers.kt
@@ -0,0 +1,14 @@
+package lotto.domain
+
+import camp.nextstep.edu.missionutils.Randoms
+import lotto.policy.GamePolicy
+
+object LottoNumbers {
+	fun generate(): List<Int> {
+		return Randoms.pickUniqueNumbersInRange(
+			GamePolicy.LOTTO_MIN_NUMBER,
+			GamePolicy.LOTTO_MAX_NUMBER,
+			GamePolicy.LOTTO_SIZE
+		)
+	}
+}
