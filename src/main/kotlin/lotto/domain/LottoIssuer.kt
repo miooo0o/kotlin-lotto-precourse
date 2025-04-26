2031afb72d7caf401b3b1c5b255743d6ee037d45
Mina Kim <mmmiiinn@me.com>
2025-04-26 22:22:48 +0200

    feat(domain): implement LottoIssuer to issue tickets based on purchase amount
    - Validates purchase amount.
    - Calculates the number of tickets to issue.
    - Issues a list of Lotto objects.

---
+++ src/main/kotlin/lotto/domain/LottoIssuer.kt
@@ -0,0 +1,31 @@
+package lotto.domain
+
+import lotto.error.PurchaseError
+import lotto.error.isStatusSuccess
+import lotto.error.toMessage
+import lotto.policy.GamePolicy
+import lotto.policy.isAtLeastTicketPrice
+import lotto.policy.isBelowMaxAllowableAmount
+import lotto.policy.isDivisibleByTicketPrice
+
+private fun Long.validateSelf() {
+	require(isAtLeastTicketPrice().isStatusSuccess()) { PurchaseError.CANNOT_AFFORD_TICKET.toMessage() }
+	require(isDivisibleByTicketPrice().isStatusSuccess()) { PurchaseError.NOT_DIVISIBLE_BY_TICKET_PRICE.toMessage() }
+	require(isBelowMaxAllowableAmount().isStatusSuccess()) { PurchaseError.AMOUNT_TOO_LARGE.toMessage() }
+
+}
+
+class LottoIssuer(private val amount: Long) {
+	init {
+		amount.validateSelf()
+	}
+
+	val ticketCount: Int
+		get() = (amount / GamePolicy.TICKET_PRICE).toInt()
+
+	fun issue(): List<Lotto> {
+		return List(ticketCount) {
+			Lotto(LottoNumbers.generate())
+		}
+	}
+}
