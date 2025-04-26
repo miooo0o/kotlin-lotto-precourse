dcdfae04317593af900d5e1bdf90b636cf0d917b
Mina Kim <mmmiiinn@me.com>
2025-04-26 21:09:01 +0200

    refactor(application): restructure input parsing order and add main process comments

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,17 +3,26 @@ package lotto
 import lotto.error.UnexpectedException
 import lotto.policy.GamePolicy
 import lotto.view.InputView
-import lotto.view.util.repeatUntilValid
+import lotto.view.repeatUntilValid
 
 fun main() {
 
 	try {
-		
-		val winning = repeatUntilValid(GamePolicy.WINNING_MESSAGE) { InputView.parseWinningNumbersOrThrow(it) }
-		val bonus = repeatUntilValid(GamePolicy.BONUS_MESSAGE) { InputView.parseBonusNumberOrThrow(it) }
+
+		// Please enter the purchase amount.
 		val amount = repeatUntilValid(GamePolicy.AMOUNT_MESSAGE) { InputView.parseAmountOrThrow(it) }
 
-	} catch (e: UnexpectedException) {
+		// -> You have purchased 8 tickets. (LottoIssuer -> Lotto: List<Int>)
 
+		// Please enter last week's winning numbers.
+		val winning = repeatUntilValid(GamePolicy.WINNING_MESSAGE) { InputView.parseWinningNumbersOrThrow(it) }
+
+		// Please enter the bonus number.
+		val bonus = repeatUntilValid(GamePolicy.BONUS_MESSAGE) { InputView.parseBonusNumberOrThrow(it) }
+
+		// Eval Lotto -> Print Winning Statistics
+
+	} catch (e: UnexpectedException) {
+		// Programme exit (1)
 	}
 }
