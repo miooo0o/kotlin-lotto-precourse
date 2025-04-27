508c7f95bb6627244211168706801abd3d7e7496
Mina Kim <mmmiiinn@me.com>
2025-04-27 17:55:43 +0200

    test(domain): re-import Lotto in LottoTest after cleanup

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -1,23 +1,24 @@
 package lotto
 
+import lotto.domain.Lotto
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class LottoTest {
-    @Test
-    fun `throws an exception when lotto numbers exceed six`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
-        }
-    }
+	@Test
+	fun `throws an exception when lotto numbers exceed six`() {
+		assertThrows<IllegalArgumentException> {
+			Lotto(listOf(1, 2, 3, 4, 5, 6, 7))
+		}
+	}
 
-    // TODO: Implement production code to pass the test
-    @Test
-    fun `throws an exception when lotto numbers contain duplicates`() {
-        assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
-        }
-    }
+	// TODO: Implement production code to pass the test
+	@Test
+	fun `throws an exception when lotto numbers contain duplicates`() {
+		assertThrows<IllegalArgumentException> {
+			Lotto(listOf(1, 2, 3, 4, 5, 5))
+		}
+	}
 
-    // TODO: Implement tests based on the added features
+	// TODO: Implement tests based on the added features
 }
