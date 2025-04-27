2232d0a1df908637e84970c799f12ec7ab5d8e4e
Mina Kim <mmmiiinn@me.com>
2025-04-27 20:32:04 +0200

    refactor(application): handle game result and exception more explicitly in Application.kt
    - Receive GameResult from GameService.start() and pass to GameService.show()
    - Separate RetryInputException and UnexpectedException handling for clearer flow
    - Add TODOs for standardizing error message format

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,13 +1,20 @@
 package lotto
 
+import lotto.error.RetryInputException
 import lotto.error.UnexpectedException
 import lotto.service.GameService
 
 fun main() {
-
 	try {
-		GameService.start()
+		val gameResult = GameService.start()
+		GameService.show(gameResult)
+	} catch (e: RetryInputException) {
+		println(e.message)
 	} catch (e: UnexpectedException) {
-		throw e
+		println("An unexpected error occurred: ${e.message}") // TODO: message should start with [ERROR]:
+		return
+	} catch (e: Exception) {
+		println("Something went wrong: ${e.message}")  // TODO: message should start with [ERROR]:
+		return
 	}
 }
