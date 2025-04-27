3e51e6d34b973bf73a1b31901c2b159b4b065b4f
Mina Kim <mmmiiinn@me.com>
2025-04-27 21:58:41 +0200

    refactor(view): decompose repeatUntilValid into smaller functions

--- src/main/kotlin/lotto/view/ViewUtils.kt
+++ src/main/kotlin/lotto/view/ViewUtils.kt
@@ -2,6 +2,10 @@ package lotto.view
 
 import lotto.error.*
 
+fun printPrompt(line: String) {
+	println(line)
+}
+
 fun <T> repeatUntilValid(
 	prompt: String,
 	reader: (String) -> T,
@@ -9,21 +13,27 @@ fun <T> repeatUntilValid(
 ): T {
 	while (true) {
 		try {
-			val input = InputView.readLineFromConsole(prompt)
-			val value = reader(input)
-			val error = validator(value)
-			if (error.isStatusFailure()) {
-				throw RetryInputException(error.toMessage())
-			}
-			return value
+			return parseInput(readUserInput(prompt), reader)
+				.also { validateInput(it, validator) }
 		} catch (e: RetryInputException) {
 			println(e.message + "\n")
-		} catch (e: UnexpectedException) {
+		} catch (e: Exception) {
 			throw e
 		}
 	}
 }
 
-fun printPrompt(line: String) {
-	println(line)
+private fun readUserInput(prompt: String): String {
+	return InputView.readLineFromConsole(prompt)
+}
+
+private fun <T> parseInput(input: String, reader: (String) -> T): T {
+	return reader(input)
+}
+
+private fun <T> validateInput(value: T, validator: (T) -> ErrorType) {
+	val error = validator(value)
+	if (error.isStatusFailure()) {
+		throw RetryInputException(error.toMessage())
+	}
 }
