b5115ce3aefc553c02fb272aca2f0d651ac644f9
Mina Kim <mmmiiinn@me.com>
2025-04-27 09:47:55 +0200

    feat(view): show purchased ticket count before listing tickets

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -3,5 +3,7 @@ package lotto.view
 import lotto.domain.Lotto
 
 fun List<Lotto>.viewPurchasedTickets() {
+	println("You have purchased ${this.size} tickets.")
 	forEach { it.display() }
+	println()
 }
