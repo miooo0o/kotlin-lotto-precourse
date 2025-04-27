69dd0f61b10ed09d1d34d390cfe0829f6c75c365
Mina Kim <mmmiiinn@me.com>
2025-04-27 20:03:55 +0200

    feat(service): modularize GameService with GameResult return
    - Refactored GameService to separate purchase, winning number reading, and evaluation flows
    - start() now returns GameResult, separating data collection and display
    - Added show(GameResult) for output responsibility

--- src/main/kotlin/lotto/service/GameService.kt
+++ src/main/kotlin/lotto/service/GameService.kt
@@ -1,7 +1,10 @@
 package lotto.service
 
 import lotto.domain.Lotto
+import lotto.domain.LottoEvaluator
 import lotto.domain.LottoIssuer
+import lotto.domain.Rank
+import lotto.error.isStatusSuccess
 import lotto.policy.GamePolicy
 import lotto.policy.doesNotOverlapWithWinningNumbers
 import lotto.policy.hasValidSize
@@ -11,14 +14,32 @@ import lotto.view.OutputView
 import lotto.view.repeatUntilValid
 
 object GameService {
-	fun start() {
+	fun start(): GameResult {
+		val (lottoList, amount) = purchaseLottos()
+		val (winningNumbers, bonusNumber) = readWinningInfo()
+		return GameResult(lottoList, winningNumbers, bonusNumber, amount)
+	}
+
+	fun show(result: GameResult) {
+		val matchResults = evaluateRanks(
+			result.lottoList, result.winningNumbers, result.bonusNumber
+		)
+		OutputView.displayMatches(matchResults)
+		OutputView.displayProfitRate(matchResults, result.amount)
+	}
+
+	private fun purchaseLottos(): Pair<List<Lotto>, Long> {
 		val amount = readPurchaseAmount()
-
 		val lottoList = issueLottoList(amount)
-		OutputView.viewPurchasedTickets(lottoList)
+		OutputView.displayPurchasedTickets(lottoList)
+		return lottoList to amount
+	}
 
+	private fun readWinningInfo(): Pair<List<Int>, Int> {
 		val winningNumbers = readWinningNumbers()
 		val bonusNumber = readBonusNumber(winningNumbers)
+		return winningNumbers to bonusNumber
+	}
 
 	// TODO: move to GameResult
 	private fun evaluateRanks(
@@ -54,10 +75,6 @@ object GameService {
 	private fun issueLottoList(amount: Long): List<Lotto> {
 		val issuer = LottoIssuer(amount)
 		return issuer.issue()
-	}
-
-	private fun viewPurchasedTickets(lottos: List<Lotto>) {
-		lottos.viewPurchasedTickets()
 	}
 
 	private fun readWinningNumbers(): List<Int> {
